8b362707f519a189431b10acc3dc432723229931
Lucasion <lucasion@hotmail.com>
2025-10-07 07:35:53 +0000

    Refactor character model management: move model definitions to a new shared file and clean up client/server scripts

--- gamemode/character/cl_character.lua
+++ gamemode/character/cl_character.lua
@@ -3,30 +3,8 @@
     Client-side character creation interface
 ]] --
 
-IonRP.Character = IonRP.Character or {}
 
--- Available character models (synced from server)
-IonRP.Character.Models = {
-  Male = {
-    "models/player/Group01/male_01.mdl",
-    "models/player/Group01/male_02.mdl",
-    "models/player/Group01/male_03.mdl",
-    "models/player/Group01/male_04.mdl",
-    "models/player/Group01/male_05.mdl",
-    "models/player/Group01/male_06.mdl",
-    "models/player/Group01/male_07.mdl",
-    "models/player/Group01/male_08.mdl",
-    "models/player/Group01/male_09.mdl",
-  },
-  Female = {
-    "models/player/Group01/female_01.mdl",
-    "models/player/Group01/female_02.mdl",
-    "models/player/Group01/female_03.mdl",
-    "models/player/Group01/female_04.mdl",
-    "models/player/Group01/female_05.mdl",
-    "models/player/Group01/female_06.mdl",
-  }
-}
+include("sh_character.lua")
 
 --[[
     Open character creation menu
--- gamemode/character/sv_character.lua
+++ gamemode/character/sv_character.lua
@@ -3,35 +3,14 @@
     Server-side character loading, saving, and creation
 ]] --
 
-IonRP.Character = IonRP.Character or {}
+include("sh_character.lua")
+AddCSLuaFile("cl_character.lua")
 
 -- Network strings
 util.AddNetworkString("IonRP_RequestCharacterCreation")
 util.AddNetworkString("IonRP_CreateCharacter")
 util.AddNetworkString("IonRP_CharacterLoaded")
 
--- Available character models
-IonRP.Character.Models = {
-  Male = {
-    "models/player/Group01/male_01.mdl",
-    "models/player/Group01/male_02.mdl",
-    "models/player/Group01/male_03.mdl",
-    "models/player/Group01/male_04.mdl",
-    "models/player/Group01/male_05.mdl",
-    "models/player/Group01/male_06.mdl",
-    "models/player/Group01/male_07.mdl",
-    "models/player/Group01/male_08.mdl",
-    "models/player/Group01/male_09.mdl",
-  },
-  Female = {
-    "models/player/Group01/female_01.mdl",
-    "models/player/Group01/female_02.mdl",
-    "models/player/Group01/female_03.mdl",
-    "models/player/Group01/female_04.mdl",
-    "models/player/Group01/female_05.mdl",
-    "models/player/Group01/female_06.mdl",
-  }
-}
 
 --[[
     Check if a player has a character
