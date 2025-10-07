714c825e9f985e716c4a0d75db3b17e77031a1c3
Lucasion <lucasion@hotmail.com>
2025-10-07 23:32:47 +0000

    Add police officer job

---
+++ gamemode/job/jobs/job_police_officer.lua
@@ -0,0 +1,14 @@
+JOB_POLICE_OFFICER = JOB:New("police_officer", "Police Officer")
+JOB_POLICE_OFFICER.color = Color(20, 150, 20)
+JOB_POLICE_OFFICER.salary = 45
+
+-- We need models...
+-- JOB_POLICE_OFFICER.playerModels
+
+JOB_POLICE_OFFICER.weapons = {
+  "mg_akilo47"
+}
+
+JOB_POLICE_OFFICER.ammo = {
+  ar2 = 240,
+}
